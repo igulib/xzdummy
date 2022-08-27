516d49d7fcc6c2f45d06c162e974ee92fa66cdee
iotanbo <iotanbo.com.t1@gmail.com>
2022-08-27 19:34:31 +0300

    fix: correctly assign github actions env vars

--- pkg/one/one.go
+++ pkg/one/one.go
@@ -2,6 +2,15 @@ package one
 
 import "fmt"
 
+func isPositive(val int) {
+	if val > 0 {
+		fmt.Println("val is positive or zero")
+	} else {
+		fmt.Println("val is negative")
+	}
+}
+
 func PackageOneHello() {
 	fmt.Println("Hello from package one!")
+	isPositive(5)
 }
